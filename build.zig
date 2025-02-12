c44dfbb131ed75edacd7dcc2e940e840dc1d1b97
Raul <95168615+raugl@users.noreply.github.com>
2025-02-13 00:00:53 +0200

    Fixed build searching for emscripten prebuilt dep

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 // TODO: Add step for generating translated C headers
 // TODO: Get rid of the external dependency `system_sdk`
+// TODO: Fix include headers
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -21,21 +22,23 @@ pub fn build(b: *std.Build) !void {
         .link_libcpp = true,
     });
 
-    { // Dependencies
-        const os = @tagName(target.result.os.tag);
-        const cpu = @tagName(target.result.cpu.arch);
-        const mode = if (optimize == .Debug) "debug" else "release";
+    if (target.result.os.tag == .emscripten) return;
 
-        var wgpu_name_buffer: [256]u8 = undefined;
-        const wgpu_dep_name = try std.fmt.bufPrint(&wgpu_name_buffer, "wgpu-{s}-{s}-{s}", .{ os, cpu, mode });
+    { // Dependencies
+        const wgpu_dep_name = b.fmt("wgpu-{s}-{s}-{s}", .{
+            .os = @tagName(target.result.os.tag),
+            .cpu = @tagName(target.result.cpu.arch),
+            .mode = if (optimize == .Debug) "debug" else "release",
+        });
 
         if (b.lazyDependency(wgpu_dep_name, .{})) |dep| {
-            if (target.result.os.tag != .emscripten) {
-                wgpu.addLibraryPath(dep.path("lib"));
-                wgpu.linkSystemLibrary("wgpu_native", .{
-                    .preferred_link_mode = if (shared) .dynamic else .static,
-                });
-            }
+            // const install_headers = b.addInstallHeaderFile(dep.path("include"), "");
+            // b.getInstallStep().dependOn(&install_headers.step);
+
+            wgpu.addLibraryPath(dep.path("lib"));
+            wgpu.linkSystemLibrary("wgpu_native", .{
+                .preferred_link_mode = if (shared) .dynamic else .static,
+            });
         }
     }
     { // Link system deps
